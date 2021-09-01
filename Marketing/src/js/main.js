ba5fe2f9027a76b6bd4f9e412b874663735dcced
Abdyl-Aziz <azizshihabov@mail.ru>
2021-09-01 16:40:30 +0200

    feat: add adaptive

--- Marketing/src/js/main.js
+++ Marketing/src/js/main.js
@@ -12,58 +12,7 @@ const materialsArticlesLinks = document.querySelectorAll('.materials__articles-l
 const materialsTabClose = document.querySelectorAll('.materials__tab-close');
 // MARKETING
 const marketingClose = document.querySelectorAll('.marketing__tab-close');
-
-marketingTabLinks.forEach(item => {
-  item.addEventListener('click', (e) => {
-    let tab = e.target.getAttribute('data-tab');
-    document.getElementById(tab).classList.add('marketing__tab--active');
-    marketing.classList.add('marketing--active');
-    materials.classList.add('materials--passive');
-
-
-    materialsTopTabs.forEach(item => {
-      item.classList.remove('materials__top-tab--active');
-    });
-
-    document.querySelector('.marketing__bottom-info').style.maxWidth = '100%';
-    document.querySelector('.marketing__top-img').style.marginLeft = '100px';
-    marketingTopInfo.classList.remove('marketing__top-info--active');
-    materials.classList.remove('materials--active');
-    marketing.classList.remove('marketing--passive');
-    marketingBottomList.style.justifyContent = 'start';
-
-
-    const linksText = [
-      'Заявки на покупку квартиры в ЖК',
-      'Заявки в бьюти-салон: М + Ж сегмент',
-      'Концепция сайта для стоматологии',
-      'Сайт для службы доставки пиццерии',
-    ];
-
-    marketingLinks[0].innerHTML = linksText[0];
-    marketingLinks[1].innerHTML = linksText[1];
-    marketingLinks[2].innerHTML = linksText[2];
-    marketingLinks[3].innerHTML = linksText[3];
-
-    marketingBottomLi.forEach(item => {
-      item.classList.remove('marketing__bottom-li--active');
-    });
-    document.querySelector('.marketing__bottom-nav').style.marginBottom = '30px';
-
-
-
-
-
-
-    marketingClose.forEach(item => {
-      item.addEventListener('click', () => {
-        document.getElementById(tab).classList.remove('marketing__tab--active');
-        marketing.classList.remove('marketing--active');
-        materials.classList.remove('materials--passive');
-      });
-    });
-  });
-});
+const wrapper = document.querySelector('.wrapper');
 
 
 for (let i = 0; i < marketingLinks.length; i++) {
@@ -72,6 +21,82 @@ for (let i = 0; i < marketingLinks.length; i++) {
     e.preventDefault();
   });
 }
+
+for(let materialsClose of materialsTabClose) {
+  materialsClose.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+}
+
+const linksText = [
+  'Заявки на покупку квартиры в ЖК',
+  'Заявки в бьюти-салон: М + Ж сегмент',
+  'Концепция сайта для стоматологии',
+  'Сайт для службы доставки пиццерии',
+];
+
+function linksTextCreator() {
+  marketingLinks[0].innerHTML = linksText[0];
+  marketingLinks[1].innerHTML = linksText[1];
+  marketingLinks[2].innerHTML = linksText[2];
+  marketingLinks[3].innerHTML = linksText[3];
+}
+
+
+const media1200 = window.matchMedia('(max-width: 1200px)');
+
+function media1200px(e) {
+  if (e.matches) {
+
+  }
+}
+
+media1200.addListener(media1200px);
+media1200px(media1200);
+
+
+
+
+marketingTabLinks.forEach(item => {
+  item.addEventListener('click', (e) => {
+    let tab = e.target.getAttribute('data-tab');
+    document.getElementById(tab).classList.add('marketing__tab--active');
+
+    console.log(window.innerWidth);
+
+    materialsTopTabs.forEach(item => {
+      item.classList.remove('materials__top-tab--active');
+    });
+
+    document.querySelector('.marketing__bottom-info').style.maxWidth = '100%';
+    marketingTopInfo.classList.remove('marketing__top-info--active');
+    marketingBottomList.style.justifyContent = 'start';
+
+    marketingBottomLi.forEach(item => {
+      item.classList.remove('marketing__bottom-li--active');
+    });
+    document.querySelector('.marketing__bottom-nav').style.marginBottom = '30px';
+
+    marketingClose.forEach(item => {
+      item.addEventListener('click', () => {
+        document.getElementById(tab).classList.remove('marketing__tab--active');
+        marketing.classList.remove('marketing--active');
+        materials.classList.remove('materials--passive');
+      });
+    });
+
+    linksTextCreator();
+
+    if (window.innerWidth > 1200) {
+      marketing.classList.add('marketing--active');
+      materials.classList.add('materials--passive');
+    }
+
+  });
+});
+
+
+
 
 materialsArticlesLinks.forEach(item => {
   item.addEventListener('click', (e) => {
@@ -83,26 +108,41 @@ materialsArticlesLinks.forEach(item => {
     marketingTabs.forEach(item => {
       item.classList.remove('marketing__tab--active');
     });
+
     marketing.classList.remove('marketing--active');
     materials.classList.remove('materials--passive');
 
-    document.querySelector('.marketing__bottom-info').style.maxWidth = '370px';
-    document.querySelector('.marketing__top-img').style.marginLeft = '100px';
-    marketingTopInfo.classList.add('marketing__top-info--active');
-    materials.classList.add('materials--active');
-    marketing.classList.add('marketing--passive');
-    marketingBottomList.style.justifyContent = 'center';
-    marketingLinks.forEach(item => {
-      item.innerHTML = '';
-    });
-    marketingBottomLi.forEach(item => {
-      item.classList.add('marketing__bottom-li--active');
-    });
-    document.querySelector('.marketing__bottom-nav').style.marginBottom = '0px';
+
+
+
+
+
+
+    if (wrapper.clientWidth > 1200) {
+      document.querySelector('.marketing__bottom-info').style.maxWidth = '370px';
+      marketingTopInfo.classList.add('marketing__top-info--active')
+      materials.classList.add('materials--active');
+      marketing.classList.add('marketing--passive');
+
+      marketingBottomList.style.justifyContent = 'center';
+      marketingLinks.forEach(item => {
+        item.innerHTML = '';
+      });
+      marketingBottomLi.forEach(item => {
+        item.classList.add('marketing__bottom-li--active');
+      });
+      document.querySelector('.marketing__bottom-nav').style.marginBottom = '0px';
+
+      console.log('yes');
+
+    }
+
+    console.log();
 
 
   });
 });
+
 
 materialsTabClose.forEach(item => {
   item.addEventListener('click', () => {
@@ -112,30 +152,16 @@ materialsTabClose.forEach(item => {
     });
 
     document.querySelector('.marketing__bottom-info').style.maxWidth = '100%';
-    document.querySelector('.marketing__top-img').style.marginLeft = '100px';
     marketingTopInfo.classList.remove('marketing__top-info--active');
     materials.classList.remove('materials--active');
     marketing.classList.remove('marketing--passive');
     marketingBottomList.style.justifyContent = 'start';
-
-
-    const linksText = [
-      'Заявки на покупку квартиры в ЖК',
-      'Заявки в бьюти-салон: М + Ж сегмент',
-      'Концепция сайта для стоматологии',
-      'Сайт для службы доставки пиццерии',
-    ];
-
-    marketingLinks[0].innerHTML = linksText[0];
-    marketingLinks[1].innerHTML = linksText[1];
-    marketingLinks[2].innerHTML = linksText[2];
-    marketingLinks[3].innerHTML = linksText[3];
 
     marketingBottomLi.forEach(item => {
       item.classList.remove('marketing__bottom-li--active');
     });
     document.querySelector('.marketing__bottom-nav').style.marginBottom = '30px';
 
-
+    linksTextCreator();
   });
 });
